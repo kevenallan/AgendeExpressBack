751564f03e22fbabfb5d2d2d4f2721ee3114a854
kevenallan <kevenallan98@gmail.com>
2021-12-09 15:37:39 -0300

    d sec

---
+++ src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Configuration/SecurityConfig.java
@@ -0,0 +1,44 @@
+package br.edu.ifpb.agendeexpress.AgendeExpress.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.AllArgsConstructor;
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Override
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/api1/**").permitAll()
+        .antMatchers("/api2/**").permitAll()
+        .antMatchers("/api3/**").permitAll();
+        
+}
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
+
+}
+
