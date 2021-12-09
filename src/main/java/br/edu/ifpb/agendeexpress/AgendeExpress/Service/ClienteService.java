b1f97242eff13a459e3d0dba5f25291555173609
kevenallan <kevenallan98@gmail.com>
2021-12-09 11:50:43 -0300

    tirando dependencia rabbitmq

--- src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Service/ClienteService.java
+++ src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Service/ClienteService.java
@@ -1,18 +1,11 @@
 package br.edu.ifpb.agendeexpress.AgendeExpress.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
 
 import br.edu.ifpb.agendeexpress.AgendeExpress.DTO.ClienteAtualizarDTO;
 import br.edu.ifpb.agendeexpress.AgendeExpress.DTO.ClienteCadastrarDTO;
