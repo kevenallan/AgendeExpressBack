7d39a5ee2581d12386a6bb3582e7ae0fb3240cc7
kevenallan <kevenallan98@gmail.com>
2021-12-20 15:10:34 -0300

    ..

--- src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Controller/HorarioController.java
+++ src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Controller/HorarioController.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
